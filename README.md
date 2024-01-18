6e4bf52f2eb778f752f2ed3a07be59993c1065da
AnOrdinaryUsername <57053268+AnOrdinaryUsername@users.noreply.github.com>
2024-01-18 15:42:55 -0800

    Create a custom action using jokeapi

---
+++ README.md
@@ -0,0 +1,16 @@
+# touko-assistant
+
+Train a model
+```
+rasa train
+```
+
+Start an action server to run [Custom Actions](https://rasa.com/docs/rasa/custom-actions)
+```
+rasa run actions
+```
+
+Interact with the latest model on the command line
+```
+rasa shell
+```
--- actions/actions.py
+++ actions/actions.py
@@ -4,24 +4,36 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
+from typing import Any, Text, Dict, List
 
-# This is a simple example for a custom action which utters "Hello World!"
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from jokeapi import Jokes
+import asyncio
+
+class ActionTellJoke(Action):
+
+    def name(self) -> Text:
+        return "action_tell_joke"
+
+    async def run(self, 
+                  dispatcher: CollectingDispatcher,
+                  tracker: Tracker,
+                  domain: Dict[Text, Any]
+                ) -> List[Dict[Text, Any]]:
+
+        category = next(tracker.get_latest_entity_values("joke_category"), 'Any')
+        j = await Jokes()
+
+        print(category)
+
+        joke = await j.get_joke(category=[category])
+
+        if joke["type"] == "single":
+            dispatcher.utter_message(text=joke["joke"])
+        else:
+            dispatcher.utter_message(text=joke["setup"])
+            dispatcher.utter_message(text=joke["delivery"])
+
+        return []
